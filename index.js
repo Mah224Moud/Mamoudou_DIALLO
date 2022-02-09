45392c0691e5cd8918e813f793d2ef018d771073
Mamoudou DIALLO <82276477+Mah224Moud@users.noreply.github.com>
2022-02-09 08:59:18 +0100

    version 1.0

---
+++ index.js
@@ -0,0 +1,50 @@
+const textAnim = document.querySelector('span');
+
+new Typewriter(textAnim, {
+    deleteSpeed: 10,
+    loop: true,
+})
+.typeString("Bonjour, je suis <span class='test' style='color: red'>Mamoudou DIALLO</span>")
+.pauseFor(300)
+.deleteChars(35)
+.typeString("Je suis <span class='test' style='color: red'>étudiant en Licence 3 Informatique</span>")
+.pauseFor(500)
+.start()
+
+
+
+
+//JQuery
+$('.btn-opened').on('click', function() {
+    $('.aff').css('display', 'none');
+    $('.btn-opened').css('display', 'none');
+    $('.cacher').css('display', 'block');
+    $('.cacher').css('transition', 'ease-in-out 1s');
+})
+
+$('.btn-closed').on('click', function() {
+    $('.btn-opened').css('display', 'flex');
+    $('.cacher').css('display', 'none');
+})
+
+
+
+
+/*********** Reponsive Menu ************/
+function redimensionner()
+{
+    var width= $(window).width();
+    if(width > 460)
+    {
+        $('.aff').css('display', 'block');
+        $('.btn-opened').css('display', 'none');
+    }
+    else
+    {
+        $('.aff').css('display', 'none');
+        $('.btn-opened').css('display', 'block');
+    }
+}
+
+$(window).resize(redimensionner);
+redimensionner();
